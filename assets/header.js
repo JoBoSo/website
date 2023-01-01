396d69a3eb8d8f9186c0bb77c7edf4654b72d5a6
James Scott <jamesbscott@hotmail.ca>
2022-12-31 20:11:35 -0500

    created header.js and called it in nav bar pages

---
+++ assets/header.js
@@ -0,0 +1,13 @@
+header.innerHTML = `
+<div id="bmb-header">
+    <h1 id="bmb-title">Big Money Bicycle Touring</h1>
+    <p id="bmb-phrase">The joy of riding one's bicycle anywhere one wishes and camping along the way is priceless.</p>
+</div>
+<div class="my-nav">
+    <a href="index.html"><b>BIKE TOURS</b></a>
+    <a href="hikes.html"><b>HIKES</b></a>
+    <a href="blog.html"><b>BLOG</b></a>
+    <a style="margin-left: auto;" href="subscribe.html"><i><b>SUBSCRIBE</b></i></a>
+    </a>
+</div>
+`;
